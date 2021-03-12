1522293c59d08e92afb186ceadf5b331ba5da45d
nathan-duff <nathan.dsmcrac@gmail.com>
2021-03-12 18:54:05 -0500

    Beans
    Added Beans

---
+++ Code/Bookstore/src/bean/AddressBean.java
@@ -0,0 +1,68 @@
+package bean;
+
+public class AddressBean {
+	private int id;
+	private String streett;
+	private String province;
+	private String country;
+	private String zip;
+	private String phone;
+
+	public AddressBean(int id, String streett, String province, String country, String zip, String phone) {
+		super();
+		this.id = id;
+		this.streett = streett;
+		this.province = province;
+		this.country = country;
+		this.zip = zip;
+		this.phone = phone;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreett() {
+		return streett;
+	}
+
+	public void setStreett(String streett) {
+		this.streett = streett;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
