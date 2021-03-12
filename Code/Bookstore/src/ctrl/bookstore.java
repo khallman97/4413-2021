2fb0b35ebe407854d92eac0c758e4985704bb6db
illusionXD <36828222+illusionXD@users.noreply.github.com>
2021-03-12 18:59:28 -0500

    Update bookstore ctrl

--- Code/Bookstore/src/ctrl/bookstore.java
+++ Code/Bookstore/src/ctrl/bookstore.java
@@ -1,11 +1,14 @@
 package ctrl;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class bookstore
@@ -22,12 +25,33 @@ public class bookstore extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    @Override
+	public void init(ServletConfig config) throws ServletException{
+		super.init(config);
+		
+//		try {
+//			getServletContext().setAttribute("bookstoreModel", bookstoreModel.getInstance());
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String target = "/SisApp.html";
+		
+		String category = "";
+		if (request.getParameter("category") != null && request.getParameter("category") != "") {
+			category = request.getParameter("category");
+		}
+		
+		request.getRequestDispatcher(target).forward(request, response);
+
 	}
 
 	/**
