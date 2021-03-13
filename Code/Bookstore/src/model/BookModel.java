afd1d4cffa67c4c25f4dd4ab4ddf0b1acc27c680
rasho95 <rasho95@my.yorku.ca>
2021-03-12 18:53:53 -0800

    basic model added.

---
+++ Code/Bookstore/src/model/BookModel.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Map;
+
+import bean.BookBean;
+
+public class BookModel {
+
+	public Map<String, BookBean> retrieveBook(String bid, String title) throws Exception {
+		return null;
+			
+	}
+	
+	public String bookAdd(String bid, String title, int price, String category, String author, String picture_link) {
+		return null;
+		
+	}
+	
+	public String bookDelete (String bid) {
+		return null;
+	}
+}
