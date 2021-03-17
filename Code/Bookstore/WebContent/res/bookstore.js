9b78e1db3105da27190b31189404e3c3348ec8c6
Kyle <khallman97@hotmail.com>
2021-03-16 22:11:15 -0400

    updated bug in bookstore.js from last commit

--- Code/Bookstore/WebContent/res/bookstore.js
+++ Code/Bookstore/WebContent/res/bookstore.js
@@ -70,15 +70,16 @@ function addToCart(){
 			var price = e.target.parentNode.getElementsByClassName("price")[0].innerHTML;
 			data ="bid=" + bid;
 			//need to refresh cart counter
+			request.open("GET", ("rest/cart/add" + "?" + data), true);
+			request.onreadystatechange = () => {
+				console.log("added to cart");
+				console.log(request.responseText);
+			};
+			request.send();
 		});
 	};
-	request.open("GET", ("rest/cart/add" + "?" + data), true);
-	request.onreadystatechange = () => {
-		console.log("Added to cart");
-		console.log(request.responseText);
-	};
-	request.send();
-	displayBooks();
+	
+	
 }
 
 
