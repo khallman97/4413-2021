e0e166ded944fc2876dc3ba310d10ff16cf895e6
nathan-duff <nathan.dsmcrac@gmail.com>
2021-03-28 23:46:50 -0400

    Added credit card info and some js validation

--- Code/Bookstore/WebContent/res/payment.js
+++ Code/Bookstore/WebContent/res/payment.js
@@ -1,11 +1,26 @@
 var counter = 0;
 
 function confirmOrder() {
-	var display = document.getElementById("mainDisplay")
-	if (counter % 3 == 2) {
-		display.innerHTML = "<p>Credit Card Authorization Failed.</p>"
-	} else {
-		display.innerHTML = "<p>Order Successfully Completed.</p>"
+	var display = document.getElementById("mainDisplay");
+	var cardString = document.getElementById("cardNum").value;
+	var CVVString = document.getElementById("CVV").value;
+	var expDate =  document.getElementById("expiration").value;
 
+	if (cardString.length < 16) {
+		display.innerHTML = "<p>insufficient characters for credit card number.</p>";
+	}
+	else if (isNaN(cardString)) {
+		display.innerHTML = "<p>credit card cannot contain non-numeric values.</p>";
+	}
+	else if (CVVString.length < 4) {
+		display.innerHTML = "<p>insufficient characters for CVV.</p>";
+	}
+	else if (isNaN(CVVString)) {
+		display.innerHTML = "<p>CVV cannot contain non-numeric values.</p>";
+	}
+	else if (counter % 3 == 2) {
+		display.innerHTML = "<p>Credit Card Authorization Failed.</p>";
+	} else {
+		display.innerHTML = "<p>Order Successfully Completed.</p>";
 	}
 }
